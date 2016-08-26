648bbd1d14143f88837aba00c4cc862b82fa0b37
Obi Symons <symons.obi@gmail.com>
2016-08-26 22:58:57 +1000

    Changed to an array

--- application.js
+++ application.js
@@ -1,14 +1,14 @@
 Array.prototype.printData = function (arrayLocation) {
   console.log(this[arrayLocation].name + " is located at these coords:\n" + this[arrayLocation].s + " S\n" + this[arrayLocation].e + " E\nIt has a rating of " + this[arrayLocation].rating + "/10!")
 };
-for (var i = 0,x = locations.length;i < x ;i++) {
-    locations.printData(i);
-};
+//for (var i = 0,x = locations.length;i < x ;i++) {
+//    locations.printData(i);
+//};
 
-// This example adds a marker to indicate the position of Bondi Beach in Sydney,
-// Australia.
 //b63b3b red
 //3b6bb6 blue
+var image = 'Images/icon.png';
+var markers = [];
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
@@ -33,30 +33,30 @@ function initMap() {
   });
 
   var image = 'Images/icon.png';
-  for (var i = 0, x = locations.length;i<x;i++) {
-    var markerPosition = {lat: locations[i].s, lng: locations[i].e};
-
-    var contentString = '<div id="content">' +
-    '<div id="siteNotice"></div>' + locations[i].name + '</h1>' +
-    '<p><b>' + locations[i].name + '</b></p><p>' + locations[i].discription + " " + locations[i].rating + '/10</p><img src="Images/Melbourne.jpg"/>' +
-    "</div></div>"
-
-    var infowindow = new google.maps.InfoWindow({
-    content: contentString
-    });
-
-    var waterMarker = new google.maps.Marker({
-          position: markerPosition,
-          map: map,
-          icon: image,
-          title: locations[i].name
-    });
-    function bindInfoWindow(marker, map, infowindow, html) {
-      google.maps.event.addListener(marker, 'click', function() {
-        infowindow.setContent(html);
-        infowindow.open(map, this);
+  var markers = [];
+  for (var i = 0; i < locations.length; i++) {
+      var contentString = '<div id="content">' +
+      '<div id="siteNotice"></div>' + locations[i].name + '</h1>' +
+      '<p><b>' + locations[i].name + '</b></p><p>' + locations[i].discription + " " + locations[i].rating + '/10</p><img src="Images/Melbourne.jpg"/>' +
+      "</div></div>";
+      var infowindow = new google.maps.InfoWindow({
+      content: contentString
       });
+      createMarkers(locations.name);
+      createContextWindows(markers[i], infowindow, contentString);
+      function createMarkers (name) {
+          markers.push(new google.maps.Marker({
+            position: {lat: locations[i].s, lng: locations[i].e},
+            map: map,
+            icon: image,
+            title: name
+          }));
+      }
+      function createContextWindows (marker, infowindow, html) {
+        google.maps.event.addListener(marker, 'click', function(){
+          infowindow.setContent(html);
+          infowindow.open(map, marker);
+        });
     }
-    bindInfoWindow(waterMarker, map, infowindow, contentString);
   }
 }
