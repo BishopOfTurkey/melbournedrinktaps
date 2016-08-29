4ffbb0bfc251fa4e0a16cbe2104d13fc008f64a8
Obi Symons <symons.obi@gmail.com>
2016-08-29 22:27:40 +1000

    shit

--- application.js
+++ application.js
@@ -48,7 +48,7 @@ function initMap() {
     for (var i = 0; i < locations.length; i++) {
       var contentString = '<div id="content">' +
       '<div id="siteNotice"></div>' +
-      '<p><b>' + locations[i].name + '</b></p><p>' + locations[i].discription + " " + locations[i].rating + '/10</p><img src="' + locations[i].image +'" width="250px">' +
+      '<p><b>' + locations[i].name + '</b></p><p>' + locations[i].discription + " " + locations[i].rating + '/10</p><div class="tapImage" ><img src="' + locations[i].image +'" width="250px"></div>' +
       "</div></div>";
       var infowindow = new google.maps.InfoWindow({
       content: contentString
